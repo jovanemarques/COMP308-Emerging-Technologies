bb1b16486447d02765bd5258e2f78356a9eb6177
Jovane Marques <jovanemarques@gmail.com>
2020-02-11 00:44:04 -0500

    week 6 stuff

---
+++ w6/SampleMidTermTest/config/env/development.js
@@ -0,0 +1,6 @@
+﻿//Development configuration options
+//To sign the session identifier, use a secret string
+module.exports = {
+    db:'mongodb://localhost/sample-midterm-test',
+    sessionSecret: 'developmentSessionSecret'
+};
